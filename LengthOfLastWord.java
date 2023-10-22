bdc014ec84f2710a424c3b63c4ec41812be789cf
Shailesh kumar <skrshailesh1996@gmail.com>
2023-10-22 18:08:39 +0530

    lengthOfLastWord

--- LengthOfLastWord.java
+++ LengthOfLastWord.java
@@ -1,2 +1,20 @@
 public class LengthOfLastWord {
+    public static void main(String[] args) {
+        String str = "fly to the moon ";
+        System.out.println(lengthOfLastWord(str));
+    }
+
+
+    public static int lengthOfLastWord(String s) {
+        String str = s.trim();
+        int count = 0;
+        for (int i = str.length() - 1; i >= 0; i--) {
+            if (str.charAt(i) != ' ') {
+                count++;
+            } else {
+                break;
+            }
+        }
+        return count;
+    }
 }

25ac9e4d37ab59e2fe7c03b42ee87550c4c5963c
Shailesh kumar <skrshailesh1996@gmail.com>
2023-11-05 19:39:54 +0530

    Contains Duplicate II

---
+++ Contains_Duplicate_II.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Contains_Duplicate_II {
+    public static void main(String[] args) {
+        int nums[] = {1, 2, 3, 1};
+        int k = 3;
+        System.out.println(containsNearbyDuplicate(nums, k));
+    }
+
+    public static boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> hm = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (!hm.containsKey(nums[i])) {
+                hm.put(nums[i], i);
+            } else {
+                int PastIndex = hm.get(nums[i]);
+                if (Math.abs(PastIndex - i) <= k) {
+                    return true;
+                } else {
+                    hm.put(nums[i], i);
+                }
+
+            }
+        }
+        return false;
+    }
+}

93a5c2fd11622b06b6eb83769e431386be4facd1
Shailesh kumar <skrshailesh1996@gmail.com>
2023-11-16 21:06:55 +0530

    IsSubsequence292

---
+++ IsSubsequence292.java
@@ -0,0 +1,29 @@
+public class IsSubsequence292 {
+    public static void main(String[] args) {
+        String s = "abc", t = "ahbgdc";
+        System.out.println(isSubsequence(s, t));
+    }
+
+    public static boolean isSubsequence(String s, String t) {
+
+        if (s.length() == 0) {
+            return true;
+        }
+        int i = 0;
+        int j = 0;
+
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+                j++;
+            } else {
+                j++;
+            }
+        }
+        if (i == s.length()) {
+            return true;
+        }
+        return false;
+    }
+
+}

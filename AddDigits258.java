eea9e091d72b47c2a4e5d973192384887a81b6fd
Shailesh kumar <skrshailesh1996@gmail.com>
2023-11-18 00:10:09 +0530

    Add Digits 258

--- AddDigits258.java
+++ AddDigits258.java
@@ -1,2 +1,13 @@
 public class AddDigits258 {
+    public static void main(String[] args) {
+        int num = 38;
+        System.out.println(addDigits(num));
+    }
+
+    public static int addDigits(int num) {
+        if (num == 0) {
+            return 0;
+        }
+        return (num % 9 == 0) ? 9 : num % 9;
+    }
 }

94c2c4625c0055da02715e431e8caa0bdc0cbff1
Shailesh kumar <skrshailesh1996@gmail.com>
2023-11-24 23:58:01 +0530

    PlusOne66

---
+++ PlusOne66.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class PlusOne66 {
+    public static void main(String[] args) {
+        int[] digits = {1, 2, 3};
+        System.out.println(Arrays.toString(plusOne(digits)));
+    }
+
+    public static int[] plusOne(int[] digits) {
+        int n = digits.length;
+        for (int i = n - 1; i > 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int[] newNumber = new int[n + 1];
+        newNumber[0] = 1;
+        return newNumber;
+    }
+}

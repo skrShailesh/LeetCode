09e706e0c4d04de97dd88e265911098836f17ec2
Shailesh kumar <skrshailesh1996@gmail.com>
2023-11-26 00:16:39 +0530

    RemoveDuplicatesfromSortedArray2

---
+++ RemoveDuplicatesfromSortedArray2.java
@@ -0,0 +1,17 @@
+public class RemoveDuplicatesfromSortedArray2 {
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 1, 2, 2, 3};
+        System.out.println(removeDuplicates(nums));
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        int i = 0;
+        for (int n : nums) {
+            if (i < 2 || n != nums[i - 2]) {
+                nums[i++] = n;
+            }
+
+        }
+        return i;
+    }
+}

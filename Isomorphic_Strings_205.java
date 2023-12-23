9beda2994a33d7de3738acb119d7bccff439849b
Shailesh kumar <skrshailesh1996@gmail.com>
2023-12-24 00:36:30 +0530

    SingleNumber_136

--- Isomorphic_Strings_205.java
+++ Isomorphic_Strings_205.java
@@ -2,25 +2,31 @@ import java.util.HashMap;
 
 public class Isomorphic_Strings_205 {
 
-    public boolean isIsomorphic(String s, String t) {
-        HashMap<Character, Character> hm = new HashMap<Character, Character>();
 
-        for (int i = 0; i < s.length(); i++) {
-            if (hm.containsKey(s.charAt(i))) {
-                {
-                    if (!hm.get(s.charAt(i)).equals(t.charAt(i))) {
-                        return false;
-                    }
-                }
-            else{
-                    if (hm.containsValue(t.charAt(i))) {
-                        return false;
-                    }
-                    hm.put(s.charAt(i), t.charAt(i));
-                }
-            }
+    public boolean isIsomorphic(String s, String t) {
+        if (s.length() != t.length()) {
             return false;
         }
-    }
 
+        HashMap<Character, Character> hm = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char charS = s.charAt(i);
+            char charT = t.charAt(i);
+
+            if (hm.containsKey(charS)) {
+                if (!hm.get(charS).equals(charT)) {
+                        return false;
+                    }
+            } else {
+                if (hm.containsValue(charT)) {
+                    return false;
+                }
+                hm.put(charS, charT);
+            }
+        }
+
+        return true;
+    }
 }
+
---
+++ SingleNumber_136.java
@@ -0,0 +1,14 @@
+public class SingleNumber_136 {
+    public static void main(String[] args) {
+        int[] nums = {4, 1, 2, 1, 2};
+        System.out.println(singleNumber(nums));
+    }
+
+    public static int singleNumber(int[] nums) {
+        int value = 0;
+        for (int i = 0; i < nums.length; i++) {
+            value = value ^ nums[i];
+        }
+        return value;
+    }
+}

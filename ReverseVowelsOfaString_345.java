ffe15b66d248c47284abe79a3b15261f44831793
Shailesh kumar <skrshailesh1996@gmail.com>
2023-12-28 20:20:49 +0530

    ReverseVowelsOfaString_345

--- ReverseVowelsOfaString_345.java
+++ ReverseVowelsOfaString_345.java
@@ -26,7 +26,8 @@ public class ReverseVowelsOfaString_345 {
     }
 
     public static boolean isVowel(char ch) {
-        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+                ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
             return true;
         }
         return false;
